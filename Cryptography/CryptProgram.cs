3c13ffb90dd497639e9b8aa74c020cf227a935cf
Raj.Kamal <Raj.Kamal@infogain.com>
2017-08-14 12:37:38 +0530

    Added Cryptography project

---
+++ Cryptography/CryptProgram.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Text;
+using System;
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+using System.Text.RegularExpressions;
+
+
+namespace Cryptography
+{
+
+    class CryptProgram
+    {
+        static void Main(string[] args)
+        {
+
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+
+            var p = new CommandLineOptions();
+
+            Regex r = new Regex("(.*)=(.*)");
+            Match m = r.Match(args[0]);
+
+            parser.AcceptEqualSignSyntaxForValueArguments = true;
+            parser.PreserveValueQuotesForEqualsSignSyntax = true;
+            parser.AllowShortSwitchGrouping = false;
+            parser.AcceptHyphen = true;
+            parser.ExtractArgumentAttributes(p);
+
+            try {
+                parser.ParseCommandLine(args);
+            }
+            catch (CommandLineException e) {
+                Console.WriteLine(e.Message);
+                parser.ShowUsage();
+            }
+            
+            parser.ShowParsedArguments();
+            var encrypter = new Encrypter();
+            string text, pass, code;
+            text = p.text; 
+            pass = p.key;
+            if (p.decrypt == false) {
+                code = encrypter.Encrypt(text, pass);
+                Console.Write("Encrypted: ");
+                Console.Write(code);
+
+            }
+            else {
+                try {
+                    code = encrypter.Decrypt(text, pass);
+                    Console.Write(code);
+                    Console.Write(code);
+
+                }
+                catch(FormatException fex){
+                    Console.WriteLine("Format Exception: text is not in ocrrect format. \r\nException message: "+fex.Message);
+                }
+                Console.Write("Decrypted: ");
+            }
+
+
+
+        }
+    }
+    class CommandLineOptions
+    {
+        [ValueArgument(typeof(string), "text", Description = "Plain/Cipher Text")]
+        public string text { get; set; }
+
+        [ValueArgument(typeof(string), "key", Description = "Encryption Key")]
+        public string key { get; set; }
+      
+        /* SwitchArguments - for on/off boolean logic */
+        [SwitchArgument('d', "decrypt", false, Description = "Set this to decrypt")]
+        public bool decrypt { get; set; }
+        /* SwitchArguments - for on/off boolean logic */
+        
+        [SwitchArgument('e', "encrypt", false, Description = "Set this to decrypt")]
+        public bool encrypt { get; set; }
+
+                     
+
+    }
+
+}
