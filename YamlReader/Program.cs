55ff6fc063fecfc6648384a2770aefb53dc95e6d
Raj.Kamal <Raj.Kamal@infogain.com>
2017-08-14 12:38:20 +0530

    Added Yaml Reader project

---
+++ YamlReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet;
+using YamlDotNet.RepresentationModel;
+
+namespace YamlReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = "Sample.yaml";
+            var reader = new StreamReader(new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite), Encoding.UTF8);
+            var yaml = new YamlStream();
+            yaml.Load(reader);
+
+            // Examine the stream
+            var mapping =
+                (YamlMappingNode)yaml.Documents[0].RootNode;
+
+            foreach (var entry in mapping.Children)
+            {
+                Console.WriteLine(((YamlScalarNode)entry.Key).Value);
+            }
+
+        }
+    }
+}
