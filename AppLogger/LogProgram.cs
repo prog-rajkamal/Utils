fc3ce57951943601fa576c578e9357baba809ec7
Raj Kamal <rajkamal6393@gmail.com>
2017-08-19 14:23:09 +0530

    Fixed logging program

--- AppLogger/LogProgram.cs
+++ AppLogger/LogProgram.cs
@@ -15,23 +15,16 @@ namespace AppLogger
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger
      (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         
-        static LogTest()
-        {
-            //XmlConfigurator.Configure();
-        }
         static void Main(string[] args)
         {
-          //  BasicConfigurator.Configure();
             Console.WriteLine("Shit is happening");
             logger.Debug("Here is a debug log.");
             logger.Info("... and an Info log.");
             logger.Warn("... and a warning.");
             logger.Error("... and an error.");
             logger.Fatal("... and a fatal error.");
-            log4net.Config.Log4NetConfigurationSectionHandler ac;
 
-            //log4net.Appender.
-            //log4net.Config.Log4NetConfigurationSectionHandler,log4net
+            // After running Check debug folder for app logs
         }
     }
 
