5bd89ad58268669e1b3f2634ec09bc449cca8a2b
Raj Kamal <rajkamal6393@gmail.com>
2018-01-22 21:53:46 +0530

    [Feat] Add support for EF Core

---
+++ AdminCrud/Migrations/20180122161630_CreateUserDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AdminCrud.Migrations
+{
+    public partial class CreateUserDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    DateOfBirth = table.Column<DateTimeOffset>(nullable: false),
+                    EmailId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    UpdatedAt = table.Column<DateTimeOffset>(nullable: true),
+                    UserRole = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
