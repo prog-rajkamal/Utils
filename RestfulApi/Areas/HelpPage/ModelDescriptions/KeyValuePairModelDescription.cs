cae7951eeabfe3d485cce42f2d00c421214b4a54
Raj Kamal <Raj.Kamal@igglobal.com>
2018-04-02 15:43:19 +0530

    WebApi: Sample project added

---
+++ RestfulApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace RestfulApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
